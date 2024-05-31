7147609bbc0dbf1b76eedb33834ab104a5159727
Arturo Aguilar Santos <210716@utxicotepec.edu.mx>
2024-05-31 14:12:02 -0600

    Creacion de un sidebar y ruta del mismo componente

--- Frontend/sistema-hospital/src/router/index.js
+++ Frontend/sistema-hospital/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import loginView from '@/components/login.vue'
 import registerUser from '@/components/registerUser.vue'
+import dashboard from '@/components/dashboard.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,7 +15,11 @@ const router = createRouter({
       path: '/register',
       name: 'register',
       component: registerUser
-    
+    },
+    {
+      path: '/dashboard',
+      name: 'dashboard',
+      component: dashboard
     }
   ]
 })
