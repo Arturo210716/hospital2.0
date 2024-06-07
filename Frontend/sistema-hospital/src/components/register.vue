208fb7a19072b940406bdc312b5c1a6e885f5a83
Arturo Aguilar Santos <210716@utxicotepec.edu.mx>
2024-06-07 12:58:48 -0600

    Correcion de tamaño de login

--- Frontend/sistema-hospital/src/components/register.vue
+++ Frontend/sistema-hospital/src/components/register.vue
@@ -15,14 +15,16 @@
         </div>
       
         <div class="flex flex-col md:flex-row justify-center items-center">
-            <div class="w-max md:w-1/2 p-4">
-                <div class="bg-white dark:bg-gray-800 p-8 rounded-lg shadow-md border dark:border-gray-700">
-                    <Personas></Personas>
-                </div>
-            </div>
-            <div class="w-max md:w-1/2 p-4">
+            
+            <div class="w-max md:w-1/4 p-4">
                 <div class="bg-white dark:bg-gray-800 p-8 rounded-lg shadow-md border dark:border-gray-700">
                     <Usuario></Usuario>
+                </div>
+            </div>
+            <div class="w-max md:w-1/4 p-4">
+                <div class="bg-white dark:bg-gray-800 p-8 rounded-lg shadow-md border dark:border-gray-700">
+                    <Personas></Personas>
+                    
                 </div>
             </div>
         </div>
