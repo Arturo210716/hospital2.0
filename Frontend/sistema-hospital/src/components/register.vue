7e3e69f24a912595383eafb4fbfd5102e6f6bf22
Arturo Aguilar Santos <210716@utxicotepec.edu.mx>
2024-06-13 14:14:27 -0600

    Correccion de diseño de citas al darle click dentro del dashboard

--- Frontend/sistema-hospital/src/components/register.vue
+++ Frontend/sistema-hospital/src/components/register.vue
@@ -21,7 +21,7 @@
                     <usuario></usuario>
                 </div>
             </div>
-            <div class="w-max md:w-1/4 p-4">
+            <div class="w-max md:w-1/8 p-4">
                 <div class="bg-white dark:bg-gray-800 p-8 rounded-lg shadow-md border dark:border-gray-700">
                     <personas></personas>
                     
--- Frontend/sistema-hospital/src/components/registrosMedicos.vue
+++ Frontend/sistema-hospital/src/components/registrosMedicos.vue
@@ -1,13 +1,8 @@
 <template>
     
   
-    <div class="flex flex-col md:flex-row justify-center items-center">
-        
-        <div class="w-max md:w-1/4 p-4">
-            <div class="bg-white dark:bg-gray-800 p-8 rounded-lg shadow-md border dark:border-gray-700">
-                <citas></citas>
-            </div>
-        </div>
+    <div>
+        <citas></citas>
     </div>
 
 </template>
