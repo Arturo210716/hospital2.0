5290cabb440e914120f68fbd6ca06e2c9c38e104
Arturo Aguilar Santos <210716@utxicotepec.edu.mx>
2024-06-20 15:16:27 -0600

    Creacion de metodo get y post para el modelo de personas

--- Backend/routes/persona.py
+++ Backend/routes/persona.py
@@ -1,8 +1,36 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
+from datetime import datetime
+from typing import Optional
 
 persona = APIRouter()
+personas=[]
 
-@persona.get("/personas")
+class model_personas(BaseModel):
+    id:int
+    nombre:str
+    primer_apellido:str
+    segundo_apellido:Optional[str]
+    edad:int
+    fecha_nacimiento: datetime
+    curp: str
+    tipo_sangre: str
+    created_at: datetime = datetime.now()
+    estatus: bool = False
 
-def helloWord():
-    return "Hello World"
+
+@persona.get('/')
+
+def bienvenido():
+    return "Bienvenido al api del sistema"
+
+@persona.get('/personas')
+
+def getPersonas():
+    return personas
+
+@persona.post('/personas')
+
+def save_persona(datos_persona:model_personas):
+    personas.append(datos_persona)
+    return "Datos guardados correctamente"
