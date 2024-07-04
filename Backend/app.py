dafe39722df4ece02cbbf88b3507c33de528ab55
Arturo Aguilar Santos <210716@utxicotepec.edu.mx>
2024-07-04 15:28:39 -0600

    Creacion de crud para modelos de usuario, persona

--- Backend/app.py
+++ Backend/app.py
@@ -2,7 +2,11 @@ from fastapi import FastAPI
 from routes.persona import persona
 from routes.usuario import usuario
 
-app=FastAPI()
+app=FastAPI(
+    title="Hospital General PrivilegeCare",
+    description="API para el almacenamiento de informacipn de un hospital"
+)
+
 app.include_router(persona)
 app.include_router(usuario)
 print("Bienvenido a mi aplicacion")
