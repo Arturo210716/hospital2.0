bdc94994a2c819be65a8e342f46a9f255677fa7a
Jose Gomez <210562@utxicotepec.edu.mx>
2024-07-18 22:18:06 -0600

    cambios

--- Backend/config/db.py
+++ Backend/config/db.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQL_ALCHEMY_DATABASE_URL = "mysql+pymysql://root:1234@localhost:3307/test"
+SQL_ALCHEMY_DATABASE_URL = "mysql+pymysql://root:1234@localhost:3306/test"
 engine = create_engine(SQL_ALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
