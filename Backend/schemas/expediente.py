0da2306ef8a02a8ba27b5a50b5f0701a3853d8c2
Justin Martin Muñoz Escorcia <210659@utxicotepec.edu.mx>
2024-08-07 14:04:51 -0600

    creacion de modelo Expediente

--- Backend/schemas/expediente.py
+++ Backend/schemas/expediente.py
@@ -2,21 +2,14 @@ from typing import List,Union
 from pydantic import BaseModel
 from datetime import datetime
 
-from models.persons import MyGenero, MySangre
 
 class ExpedienteBase(BaseModel):
     
-    Titulo_Cortesia:str
-    Nombre: str
-    Primer_Apellido: str
-    Segundo_Apellido: str
-    Curp: str
-    Genero: MyGenero
-    Tipo_Sangre:  MySangre  
-    Fecha_Nacimiento: datetime
-    Fotografia:str
-    Telefono: str
-    Correo_Electronico: str
+    Hora_Consulta:datetime
+    Hora_Consulta: time
+    Diagnostico: str
+    Tratamiento_Relacionado: str
+    Observaciones: str
     Estatus:bool
     Fecha_Registro:datetime
     Fecha_Actualizacion:datetime
